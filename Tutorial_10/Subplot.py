242e66398d96a1daacbaf3824d210dc66871db11
vivalka <60872424+vivalka@users.noreply.github.com>
2022-12-18 15:31:18 +0530

    Add files via upload

---
+++ Tutorial_10/Subplot.py
@@ -0,0 +1,36 @@
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+plt.style.use('seaborn')
+
+data = pd.read_csv('subplotData.csv')
+ages = data['Age']
+dev_salaries = data['All_Devs']
+py_salaries = data['Python']
+js_salaries = data['JavaScript']
+
+fig1 ,ax1 = plt.subplots()
+fig2 , ax2 = plt.subplots()
+ax1.plot(ages, py_salaries, label='Python')
+ax1.plot(ages, js_salaries, label='JavaScript')
+
+ax2.plot(ages, dev_salaries, color='#444444',
+         linestyle='--', label='All Devs')
+
+ax1.legend()
+
+ax1.set_title('Median Salary (USD) by Age for Python , JS')
+ax1.set_xlabel('Ages')
+ax1.set_ylabel('Median Salary (USD)')
+
+ax2.legend()
+
+ax2.set_title('Median Salary (USD) by Age for All devs')
+ax2.set_xlabel('Ages')
+ax2.set_ylabel('Median Salary (USD)')
+
+
+plt.tight_layout()
+
+plt.show()
